8f277da33122fc495db09ceb07097668bdc78b7a
MasterPNJ <38404325+MasterPNJ@users.noreply.github.com>
2024-08-09 17:59:18 +0200

    Fix extinguished fire indoors
    fixed the problem of the fire not being extinguished automatically indoors when it rains

--- dist/Assemblies/JobGiver_Work_TryIssueJobPackage_Patch.cs
+++ dist/Assemblies/JobGiver_Work_TryIssueJobPackage_Patch.cs
@@ -9,10 +9,16 @@ public static class JobGiver_Work_TryIssueJobPackage_Patch
     public static void Postfix(Pawn pawn, JobIssueParams jobParams, ref ThinkResult __result)
     {
         Log.Message("Postfix called for JobGiver_Work.TryIssueJobPackage");
-        if (__result.Job != null && __result.Job.def == JobDefOf.BeatFire && pawn.Map.weatherManager.RainRate > 0.5f)
+        if (__result.Job != null && __result.Job.def == JobDefOf.BeatFire)
         {
-            Log.Message("Preventing firefighting job during rain");
-            __result = ThinkResult.NoJob;
+            bool isOutside = pawn.Map.roofGrid.RoofAt(__result.Job.targetA.Cell) == null;
+            bool isRaining = pawn.Map.weatherManager.RainRate > 0.5f;
+
+            if (isRaining && isOutside)
+            {
+                Log.Message("Preventing firefighting job during rain for outdoor fire");
+                __result = ThinkResult.NoJob;
+            }
         }
     }
 }
